f4d22f57d3c86c8c38a9a7f493be370c89d6f70d
GcNoteX <gcha0054@student.monash.edu>
2026-03-27 15:35:53 +1100

    Character Spawn, Camera Built Into Character, Some Manager modification for player, level

---
+++ 00_globals/global_player_manager.gd
@@ -0,0 +1,40 @@
+extends Node
+
+const PLAYER = preload("res://characters/player/player_character.tscn")
+var player : Player
+var player_spawned : bool = false
+
+signal camera_shook( trauma : float )
+signal interact_pressed
+
+func _ready() -> void:
+	add_player_instance()
+	await get_tree().create_timer(0.2).timeout
+	player_spawned = true
+
+#region Player Instantion and Transitions
+func add_player_instance() -> void:
+	player = PLAYER.instantiate()
+	add_child( player )
+
+func set_player_position( _new_pos : Vector2 ) -> void:
+	player.global_position = _new_pos
+	pass
+
+func set_as_parent( _p : Node2D ) -> void:
+	if player.get_parent():
+		player.get_parent().remove_child( player )
+	_p.add_child( player )
+
+func unparent_player( _p : Node2D ) -> void:
+	_p.remove_child( player )
+
+#endregion
+
+#region Camera
+func shake_camera( trauma : float = 1 ) -> void:
+	camera_shook.emit( clampi( trauma, 0, 3 ) )
+#endregion
+
+func interact() -> void:
+	interact_pressed.emit()
