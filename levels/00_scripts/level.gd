a97baa6f47962598d94b77c514ffdd79cf3627ca
GcNoteX <gcha0054@student.monash.edu>
2026-04-20 12:20:03 +1000

    Simple Audio Manager Implemented

--- levels/00_scripts/level.gd
+++ levels/00_scripts/level.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 	self.y_sort_enabled = true
 	PlayerManager.set_as_parent( self )
 	LevelManager.level_load_started.connect( _free_level )
-	#AudioManager.play_music( music )
+	AudioManager.play_music( music )
 
 
 
