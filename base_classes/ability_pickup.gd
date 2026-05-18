6f23bca8190a044e38da903954ec18b666296420
Nemo115 <43259251+Nemo115@users.noreply.github.com>
2026-05-18 23:20:00 +1000

    deleted text variable

--- base_classes/ability_pickup.gd
+++ base_classes/ability_pickup.gd
@@ -2,7 +2,6 @@ extends Area2D
 class_name AbilityPickup
 
 @export var ability_scene: PackedScene
-@export var ability_text: String
 
 func get_ability() -> Ability:
 	return ability_scene.instantiate()
