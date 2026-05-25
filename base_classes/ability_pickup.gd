ed37c0699dbf9791ca4716bfd4cf17155fa48e43
Nemo115 <43259251+Nemo115@users.noreply.github.com>
2026-05-25 10:11:48 +1000

    latest changes

--- base_classes/ability_pickup.gd
+++ base_classes/ability_pickup.gd
@@ -3,5 +3,13 @@ class_name AbilityPickup
 
 @export var ability_scene: PackedScene
 
+@onready var label: Label = $Label
+
 func get_ability() -> Ability:
 	return ability_scene.instantiate()
+
+func open_label():
+	label.visible = true
+
+func close_label():
+	label.visible = false
